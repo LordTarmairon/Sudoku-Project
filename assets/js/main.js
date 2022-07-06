af07a87e81a862a72f1fac05bd8fb581d88340fa
Jose Luis <joseluis_c_garcia@hotmail.com>
2022-07-06 11:57:33 -0700

    Fixed some bugs in the code and added some new features

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,21 @@
 $(document).ready(function (){
     $(".alert-username").hide();
     $("#userNameModal").modal("show");
+    $("#username").focus();
 });
+
+$(".onGame").prop("disabled", true);
+
+const inputArray = [];
+var solved = [];
+var minutes = 0;
+var seconds = 0;
+var tens = 0;
+var Interval;
+var level = "";
+var name = "";
+var numbersArray = [];
+var totalInputs = 81;
 
 //Function for get the user name
 $("#btn-getUser").on("click", function() {
@@ -21,12 +35,7 @@ function getUserName(){
     $("#userNameModal").modal("hide");
     return true;
 }
-var minutes = 0;
-var seconds = 0;
-var tens = 0;
-var Interval;
-var level = "";
-var name = "";
+
 
 $(".start-button").on("click", function(){
     initTimer();
@@ -42,13 +51,18 @@ $(".stop-button").on("click", function(){
 });
 
 $(".button-reset").on("click", function(){
+    restartTimer();
+});
+
+//This function restore the timer to 00:00
+function restartTimer(){
     clearInterval(Interval);
     tens = "00";
     seconds = "00";
     minutes = "00";
     $("#tens").text(tens);
     $("#seconds").text(seconds);
-});
+}
 
 function startTimer() {
     tens++;
@@ -76,7 +90,6 @@ function startTimer() {
     }
 }
 
-const inputArray = [];
 function createBox() {
     for (let n = 1; n <= 9; n++) {
         let squareDiv = $("<div class=squareBox></div>");
@@ -86,9 +99,6 @@ function createBox() {
     $("#tableGame").addClass("outsideDiv");
 }
 
-createBox();
-assignInputArray();
-assignInputBox(inputArray);
 function assignInputArray() {
     for (let i = 0; i < 9; i++) {
         let row = [];
@@ -141,17 +151,38 @@ function assignInputBox(elements) {
 //This function will validate the sudoku table and take the time.
 $(".validate").on("click", function(){
     $(".totalTime").html("");
+    $("#head-score").html("");
+    $(".inputBox").prop("disabled", true);
+    $(".inputBox").removeClass("showRowCol");
 
     //Stop the timer
     clearInterval(Interval);
     let minutes = $("#minutes").text();
     let seconds = $("#seconds").text();
     let tens = $("#tens").text();
+    let content ="";
     $(".btn-level").prop("disabled", false);
 
+    let result = checkAll();
+    localStorage.setItem(parseInt(localStorage.length+1), JSON.stringify(result));
+
+    let text = $("<p class='text-black'></p>")
+    if(result.status){
+        $("#head-score").append("Congratulations! <b>"+result.user+"</b> &#127881; &#127882;");
+        $(".head-result").addClass("alert alert-success");
+        content = "You have used to carry out the sudoku: <b>"+minutes+":"+seconds+":"+tens+"</b> <br/><br/> Your Score is: <b>"+result.score+"</b></p><p class='fst-italic alert alert-info'>"+result.message+" </p>";
+
+    } else {
+        $("#head-score").append("You didn't get it! <b>"+result.user+"</b> &#10060;")
+        $(".head-result").addClass("alert alert-danger");
+        content = "You have used to carry out the sudoku: <b>"+minutes+":"+seconds+":"+tens+"</b> <br/><br/> Your Score is: <b>"+result.score+"</b></p><p class='fst-italic alert alert-warning'>"+result.message+" </p>";
+    }
+    text.append(content);
+    $(".totalTime").append(text);
+
     //let sudoku = checkNumbers()
-    $(".totalTime").append("<p class='alert alert-primary text-black'>You have used to carry out the sudoku: <b>"+minutes+":"+seconds+":"+tens+"</b></p>");
     $("#checkSudoku").modal("show");
+
 
 });
 
@@ -160,9 +191,23 @@ $(".validate").on("click", function(){
 //When the buttons with the class level are clicked we get the level send to the api and get the numbers
 //then print the numbers insede the boxes and get the solution
 $(".level").on("click", function (){
+    $(".inputBox").prop("disabled", false);
+    $(".inputBox").removeClass("bg-secondary");
+    $(".inputBox").addClass("bg-light");
+
     $("h1").eq(0).text($(this).text());
     level = $(this).text().toLowerCase();
+    level = level.trim();
 
+    //Dino animation
+    $("#dino").animate({
+        left:"100%"},  2000);
+    $("#dino").animate({
+        heigth: "toggle"});
+    $("#dino").animate({
+        left:"-11%"});
+
+    //Ajax information
     $.ajax({
         url: "https://sugoku.herokuapp.com/board?difficulty="+level,
         method: "GET",
@@ -170,7 +215,10 @@ $(".level").on("click", function (){
         dataType: 'json',
         success:function (result){
             $(".btn-level").prop("disabled", true);
+            numbersArray = result.board;
             showNumbers(result.board);
+            $(".onGame").prop("disabled", false);
+
             //Start Timer
             initTimer();
             result = {board: JSON.stringify(result.board)}
@@ -184,15 +232,19 @@ $(".level").on("click", function (){
 
 //This function print the numbers inside the boxes
 function showNumbers(numbers){
+    //Clean the showRolCol Styles
+    $(".inputBox").removeClass("showRowCol");
     for(var i = 0; i<inputArray.length; i++){
         for(var j = 0; j< inputArray[i].length; j++){
             if($(numbers)[i][j] !== 0){
+                totalInputs--;
                 $(inputArray[i][j]).val($(numbers)[i][j]);
                 $(inputArray[i][j]).prop("disabled", true)
                 $(inputArray[i][j]).removeClass("bg-light");
                 $(inputArray[i][j]).addClass("bg-secondary");
             } else {
                 $(inputArray[i][j]).val("");
+                $(inputArray[i][j]).addClass("bg-light");
                 $(inputArray[i][j]).prop("disabled", false)
             }
         }
@@ -200,24 +252,224 @@ function showNumbers(numbers){
 }
 
  //Show Row Column Box
- $("input").focus(function (event) {
-    $("input").removeClass("showRowCol");
-    var chooseBox = event.target;
-    var position = $(chooseBox).attr("data-input").split("-");
-    for (var i = 0; i < 9; i++) {
-        inputArray[position[0]][i].addClass("showRowCol");
-        inputArray[i][position[1]].addClass("showRowCol");
-    };
-    var parentBox = $(chooseBox).parent().attr("id").split("-");
-    $("#box-" + parentBox[1] + " input").addClass("showRowCol");
+ $(document).on("focus", ".inputBox", function (event) {
+     if(level === ""  || level === "easy"){
+         $(".inputBox").removeClass("showRowCol");
+         var chooseBox = event.target;
+         var position = $(chooseBox).attr("data-input").split("-");
+         for (var i = 0; i < 9; i++) {
+             inputArray[position[0]][i].addClass("showRowCol");
+             inputArray[i][position[1]].addClass("showRowCol");
+         }
+         var parentBox = $(chooseBox).parent().attr("id").split("-");
+         $("#box-" + parentBox[1] + " input").addClass("showRowCol");
+     }
 });
 
 /* Set the width of the side navigation to 250px */
-function openNav() {
+$("#btn-sideMenu").on("click", function () {
     $("#mySidenav").css("width", "250px");
-}
+});
 
 /* Set the width of the side navigation to 0 */
-function closeNav() {
+$("#btn-clssideMenu").on("click", function () {
     $("#mySidenav").css("width", "0");
+});
+
+
+//This button gonna clear the game table, reset the time
+$(".new").on("click", function(){
+    let inputBox = $(".inputBox");
+    $(".onGame").prop("disabled", true);
+    $(".onGame:first-child, .validate, .onGame:last-child").prop("disabled", false);
+
+    totalInputs = 81;
+
+    restartTimer();
+    level = "";
+
+    $("h1").eq(0).text("Timer");
+    inputBox.removeClass("bg-secondary");
+    inputBox.removeClass("bg-light");
+    inputBox.addClass("bg-light");
+    inputBox.removeClass("bg-warning");
+    inputBox.val("");
+    inputBox.prop("disabled", false);
+    $(".btn-level").prop("disabled", false);
+});
+
+//This function reset your numbers and reset the timer
+$(".reset").on("click", function() {
+    $(".inputBox").removeClass("bg-warning");
+    $(".validate").prop("disabled", false);
+    showNumbers(numbersArray);
+    restartTimer();
+    initTimer();
+});
+
+//Event for the button resolve sudoku
+$(".solve").on("click", function(){
+    $(".inputBox").removeClass("bg-warning");
+    $(".validate").prop("disabled", true);
+    totalInputs = 81;
+    showNumbers(solved);
+    restartTimer();
+});
+
+function compare(a, b) {
+    if (a.score < b.score ) {
+        return -1;
+    }
+    if (a.score > b.score) {
+        return 1;
+    }
+    return 0;
 }
+
+$(".scores").on("click", function(){
+    let total = 1;
+    $(".list-scores").html("");
+    let scores = [];
+    for(var i = 1; i <=localStorage.length; i++){
+        scores.push(JSON.parse(localStorage[i]));
+    }
+    scores.sort(compare);
+
+    for(var j = scores.length-1; j >= 0; j--){
+        let tr = $("<tr></tr>");
+        let tdN = $("<td></td>");
+        let tdSt = $("<td></td>");
+        let tdSc = $("<td></td>");
+        let tdL = $("<td></td>");
+        let tdP = $("<td></td>");
+
+        tdP.append(total);
+        tdN.append(scores[j].user);
+        tdSt.append(scores[j].status);
+        tdSc.append(scores[j].score);
+        tdL.append(scores[j].level);
+
+        tr.append(tdP);
+        tr.append(tdN);
+        tr.append(tdSt);
+        tr.append(tdSc);
+        tr.append(tdL);
+        $(".list-scores").append(tr);
+        total++;
+    }
+    $("tbody tr").eq(0).addClass("list-group-item-success");
+    $("#modal-scores").modal("show");
+});
+//This function read the inputs data and evaluate the score
+function checkAll (){
+    let count = 0;
+    var inputsTrue = totalInputs;
+    let totalScore = 0;
+    console.log(inputsTrue)
+
+    for(var n = 0; n<inputArray.length; n++){
+        for(var j = 0; j< inputArray[n].length; j++){
+            if($(inputArray[n][j]).val() != ""){
+                var rest = true;
+                var inp =inputArray[n][j];
+
+                var inputData = $(inp).attr("data-input").split("-");
+                inputArray[inputData[0]].splice(inputData[1], 1, inp);
+                let inputBox = $(inp).parent();
+                //Check if the value exist in the same row
+                inputArray[inputData[0]].forEach(function(element, key){
+                    if($(inp).val() !== "" && $(element).val() === $(inp).val() && $(element).attr("data-input") !== $(inp).attr("data-input")){
+                        $(inp).removeClass("bg-light");
+                        $(inp).addClass("bg-warning");
+                        $(element).addClass("bg-warning")
+                        rest = false;
+                        if(count !== 1 && count !== 3 && count !== 5 && count !== 7){
+                            count += 1;
+                        }
+                    }
+                });
+
+                //Check if the value exist in the same col
+                for(var i = 0; i< inputArray.length ; i++){
+                    var element = inputArray[i][inputData[1]];
+                    if($(element).val() === undefined){
+                        console.log($(element).val())
+                        return;
+                    }
+                    if($(inp).val() !== "" && $(element).val() === $(inp).val() && $(element).attr("data-input") !== $(inp).attr("data-input")){
+                        $(inp).removeClass("bg-light");
+                        $(inp).addClass("bg-warning");
+                        $(element).addClass("bg-warning");
+                        rest = false;
+
+                        if(count !== 2 && count !== 3 && count !== 6 && count !== 7){
+                            count += 2;
+                        }
+                    }
+                }
+
+                inputBox.children().each(function(index, element){
+                    //Check if the element exist in the box and if the element es different to empty and if element and event have different index
+                    if($(inp).val() !== "" && element.value === $(inp).val() && $(element).index() !== $(inp).index()){
+                        $(inp).removeClass("bg-light");
+                        $(inp).addClass("bg-warning");
+                        $(element).addClass("bg-warning");
+                        rest = false;
+                        if(count !== 4 && count !== 5 && count !== 6 && count !== 7){
+                            count += 4;
+                        }
+                    }
+                });
+                if(!rest && !$(inp).prop("disabled")){
+                    console.log($(inp).prop("disabled"));
+                    inputsTrue--;
+                }
+            } else {
+                console.log("Entre en el blanco")
+                inputsTrue--;
+            }
+        }
+    }
+    console.log(inputsTrue)
+   // console.log(totalInputs)
+   // console.log((inputsTrue*100/totalInputs).toFixed(2))
+
+
+
+    console.log(count)
+    switch (count){
+        case 0:
+            return {status: true, user:name, level:$("h1").eq(0).text(), score:totalScore+"%", message: "Congratulations! you have successfully completed the Sudoku"};
+            break;
+        case 1:
+            return {status: false, user:name, level:$("h1").eq(0).text(), score:totalScore+"%", message: "You have same values in the same Row"};
+            break;
+        case 2:
+            return {status: false, user:name, level:$("h1").eq(0).text(), score:totalScore+"%", message: "You have same values in the same Column"};
+            break;
+        case 4:
+            return {status: false, user:name, level:$("h1").eq(0).text(), score:totalScore+"%", message: "You have same values in the same Box"};
+            break;
+        case 3:
+            return {status: false, user:name, level:$("h1").eq(0).text(), score:totalScore+"%", message: "You have same values in the same Row and Column"};
+            break;
+        case 5:
+            return {status: false, user:name, level:$("h1").eq(0).text(), score:totalScore+"%", message: "You have same values in the same Row and Box"};
+            break;
+        case 6:
+            return {status: false, user:name, level:$("h1").eq(0).text(), score:totalScore+"%", message: "You have same values in the same Column and Box"};
+            break;
+        case 7:
+            return {status: false, user:name, level:$("h1").eq(0).text(), score:totalScore+"%", message: "You have same values in the same Row, Column and Box"};
+            break;
+        default:
+            return {status: false, user:name, level:$("h1").eq(0).text(), score:totalScore+"%", message: "Your sudoku is empty..."};
+            break;
+    }
+}
+
+
+
+createBox();
+assignInputArray();
+assignInputBox(inputArray);
